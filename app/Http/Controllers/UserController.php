d89cbb3d283fdfca361b679ed18cc289d3a95579
NinaDusova <ninadusova7@gmail.com>
2024-11-21 20:04:08 +0100

    adding views - home page - user login

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function login()
+    {
+        return view('user.login');
+    }
+
+    public function page()
+    {
+        return view('user.page');
+    }
+}
